b7da44f33c84d3a52e2096610101c10f009f27a1
saitripuralekkala <45476807+saitripuralekkala@users.noreply.github.com>
2020-06-23 21:45:43 +0530

    Add files via upload

---
+++ numberPattern.java
@@ -0,0 +1,21 @@
+class numberPattern
+{
+    public static void main(String[] args)
+    { 
+        int n,i, j,num; 
+    
+        for(i=0; i<n; i++) 
+        { 
+            num=1; 
+            for(j=0; j<=i; j++) 
+            {  
+                System.out.print(num+ " "); 
+                num++; 
+            } 
+    
+   
+            System.out.println(); 
+        } 
+    }
+} 
+       
